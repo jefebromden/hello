60dc35cdf3a4792f6452f5e08bb73bed75d44781
jeff <johnjeff55@gmail.com>
2022-11-09 11:23:06 -0800

    Remove single-use constants

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-const jeff = document.getElementById("jeff")
-const wendy = document.getElementById("wendy")
+// ISSUE: _Single-use_ constants are redundant.
+// ============================================
+// Both "const jeff" and "const wendy" are only used once.
+// So the "dry" good-practice principle (don't repeat yourself)
+// would suggest that we just use their values as inline
+// as seen below.
 
 function capitalize(string) {
-    const firstLetter   = string.charAt(0).toUpperCase();
-    const restOfLetters = string.slice(1);
-
-    return firstLetter + restOfLetters;
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-function sayHi(person){
-    var whois = person.getAttribute("id");
-
-    name = capitalize(whois);
-    alert(`Hi ${name}!`);
+function sayHi(person) {
+  alert(`Hi ${capitalize(person.getAttribute('id'))}!`)
 }
 
-jeff.onclick = function() {
-    sayHi(this);
-    return false;
-};
+document.getElementById('jeff').onclick = function () {
+  sayHi(this)
+  return false
+}
 
-wendy.onclick = function() {
-    sayHi(this);
-    return false;
-};
+document.getElementById('wendy').onclick = function () {
+  sayHi(this)
+  return false
+}
