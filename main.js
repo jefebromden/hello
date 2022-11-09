960423fd6cc5372ad3484508134f25dc8a00d0df
jeff <johnjeff55@gmail.com>
2022-11-09 10:01:16 -0800

    Fix: bad-practice error in sayHi()

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-const jeff = document.getElementById("jeff")
-const wendy = document.getElementById("wendy")
+const jeff = document.getElementById('jeff')
+const wendy = document.getElementById('wendy')
 
 function capitalize(string) {
-    const firstLetter   = string.charAt(0).toUpperCase();
-    const restOfLetters = string.slice(1);
+  const firstLetter = string.charAt(0).toUpperCase()
+  const restOfLetters = string.slice(1)
 
-    return firstLetter + restOfLetters;
+  return firstLetter + restOfLetters
 }
 
-function sayHi(person){
-    var whois = person.getAttribute("id");
+function sayHi(person) {
+  var whois = person.getAttribute('id')
 
-    name = capitalize(whois);
-    alert(`Hi ${name}!`);
+  // Issue: Although this is not strictly an "error" in the sense
+  // that it will not run, using undelared variables inside
+  // functions is widely considered to be bad practice.
+  // This is because undeclared variables are "hoisted" into
+  // the global space, and can produce unexpected (and hard to
+  // debug) results.
+
+  // Solution: Replace "name = capitalize(whois)" with:
+  var name = capitalize(whois)
+  alert(`Hi ${name}!`)
 }
 
-jeff.onclick = function() {
-    sayHi(this);
-    return false;
-};
+jeff.onclick = function () {
+  sayHi(this)
+  return false
+}
 
-wendy.onclick = function() {
-    sayHi(this);
-    return false;
-};
+wendy.onclick = function () {
+  sayHi(this)
+  return false
+}
