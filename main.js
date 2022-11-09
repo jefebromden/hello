286f83a7670070aabff23241fe2ba75479e31585
jeff <johnjeff55@gmail.com>
2022-11-09 14:59:00 -0800

    Remove capitalize() function

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-const jeff = document.getElementById("jeff")
-const wendy = document.getElementById("wendy")
+// The capitalize() function is currently used
+// to capitalize the first letter of the html
+// ids, "jeff" and "wendy"
 
-function capitalize(string) {
-    const firstLetter   = string.charAt(0).toUpperCase();
-    const restOfLetters = string.slice(1);
+// But there's no rule that prevents us from
+// using capital letters in an html id.
 
-    return firstLetter + restOfLetters;
-}
+// If we capitalize these html ids (i.e. Jeff | Wendy),
+// and update the css, we can:
+//  1. remove the capitalize() function entirely,
+//  2. further simplify the sayHi() function, and
+//  3. improve both the efficiency and the elegance of the code.
+//
+// I've left in the commented-out code for comparison.
 
-function sayHi(person){
-    var whois = person.getAttribute("id");
+const jeff = document.getElementById('Jeff')
+const wendy = document.getElementById('Wendy')
 
-    name = capitalize(whois);
-    alert(`Hi ${name}!`);
-}
+// const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1)
 
-jeff.onclick = function() {
-    sayHi(this);
-    return false;
-};
+// const sayHi = (personId) => alert(`Hi ${capitalize(personId)}!`)
+const sayHi = (personId) => alert(`Hi ${personId}!`)
 
-wendy.onclick = function() {
-    sayHi(this);
-    return false;
-};
+jeff.onclick = () => sayHi(jeff.id)
+wendy.onclick = () => sayHi(wendy.id)
